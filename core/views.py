8ad0c798607fad2773a4906f1cc018328c75c02a
Lucas-G0s <galvao.batista@escolar.ifrn.edu.br>
2026-07-05 15:12:21 -0300

    projeto_1 beta/2

--- core/views.py
+++ core/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Area, PublicoAlvo, Curso
+from .forms import AreaForm, PublicoAlvoForm, CursoForm
+from django.contrib.auth import authenticate, login, logout
+# Usuários
+from .models import Usuario
+from .forms import UsuarioFormCadastro
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def dashboard(request):
     return render(request, 'dashboard.html')
 
 def cadastro(request):
+    form = UsuarioFormCadastro(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    context = {
+        'form':form
+    }
     return render(request, 'cadastro.html')
 
 def curso_detalhes(request):
