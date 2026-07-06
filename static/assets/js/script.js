b383764c684a52b85125c7543c9ab210f9ae5793
Lucas-G0s <galvao.batista@escolar.ifrn.edu.br>
2026-07-05 22:06:47 -0300

    Delete static/assets directory

--- static/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const mobileNav = document.querySelector('.mobile-nav');
-
-hamburger.addEventListener('click', () => {
-  const open = hamburger.classList.toggle('is-open');
-  mobileNav.classList.toggle('is-open', open);
-  hamburger.setAttribute('aria-expanded', String(open));
-});
